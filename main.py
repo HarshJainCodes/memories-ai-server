8e0d9d7b318475ec4e62a5a6d0cc998424b90fdc
Harsh Jain <harshjain17may@gmail.com>
2026-06-21 18:47:07 +0530

    use https for localhost

--- main.py
+++ main.py
@@ -13,6 +13,14 @@ logger = logging.getLogger("uvicorn.error")
 if __name__ == "__main__":
 	if os.getenv("ENV") == "DEVELOPMENT":
 		logger.info("Running in Development Mode")
-		uvicorn.run("src.main:app", host="0.0.0.0", port=8000, reload=True)
+		uvicorn.run(
+			"src.main:app",
+			host="0.0.0.0",
+			port=8000,
+			reload=True,
+			ssl_certfile="localhost.pem",
+			ssl_keyfile="localhost-key.pem",
+		)
 	else:
+		logger.info("Running in Production Mode")
 		uvicorn.run("src.main:app", host="0.0.0.0", port=8000)
